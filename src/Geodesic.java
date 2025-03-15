33bd43b6647f99d18392dc58e58fcdd180b12ea4
CoCoSol007 <solois.corentin@gmail.com>
2025-03-15 23:03:59 +0100

    Added diameter support to geodesic system

--- src/Geodesic.java
+++ src/Geodesic.java
@@ -4,14 +4,16 @@ package src;
 ///
 /// In the Poincaré disk model a geodesic can be described by a circle orthogonal to the unit circle.
 /// So it can be described by an equation x² + y² + ax + by + 1 = 0.
-///
-/// TODO: This geodesic do not handle diameters yet.
 public class Geodesic {
     /// One parameter of the geodesic
     double a;
 
     /// One parameter of the geodesic
     double b;
+
+    /// True if the geodesic is a diameter
+    /// If so the geodesic is defined by the equation ax+by = 0
+    boolean diameter = false;
 
     /// Constructor of the geodesic
     public Geodesic(double a, double b) {
@@ -25,14 +27,18 @@ public class Geodesic {
     /// It works by solving the system of equations x² + y² + ax + by + 1 = 0
     /// with (x,y) = u and (x,y) = v
     public static Geodesic from_two_points(Point u, Point v) {
-        double A = -1 - (u.x * u.x) - (u.y * u.y);
-        double B = -1 - (v.x * v.x) - (v.y * v.y);
-
         double det = (u.x * v.y) - (u.y * v.x);
 
         if (det == 0) {
-            throw new IllegalArgumentException("u and v must be linearly independent");
+            // The geodesic is a diameter
+            Geodesic geodesic = new Geodesic(u.y - v.y, v.x - u.x);
+            geodesic.diameter = true;
+            return geodesic;
+
         }
+
+        double A = -1 - (u.x * u.x) - (u.y * u.y);
+        double B = -1 - (v.x * v.x) - (v.y * v.y);
 
         return new Geodesic((A * v.y - B * u.y) / det, (B * u.x - A * v.x) / det);
     }
@@ -44,9 +50,16 @@ public class Geodesic {
     /// x² + y² + ax + by + 1 = 0 (with (x,y) = u)
     /// (t1,t2)⋅(x,y) = 0 (where (t1,t2) is the tangent and (x,y) a segment of the circle formed by the geodesic)
     public static Geodesic from_point_and_tangent(Point u, double t1, double t2) {
+        if (t1 != 0 || t2 != 0) {
+            throw new IllegalArgumentException("Vector tangent must be not null");
+        }
+
         double det = t1 * u.y - t2 * u.x;
         if (det == 0) {
-            throw new IllegalArgumentException("t1 and t2 must be linearly independent");
+            // The geodesic is a diameter
+            Geodesic geodesic = new Geodesic(-t2,t1);
+            geodesic.diameter = true;
+            return geodesic;
         }
 
         double x = -2*(t1 * u.x + t2 * u.y) * u.y + (u.x*u.x + u.y*u.y +1)*t2;
