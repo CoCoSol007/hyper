551bd4b10f68cb8d7299b425a2a15f7624b2818e
CoCoSol007 <solois.corentin@gmail.com>
2025-03-19 18:54:43 +0100

    Add conversion methods between Point and Complex number

--- src/hyperbolic/Point.java
+++ src/hyperbolic/Point.java
@@ -1,9 +1,13 @@
 package src.hyperbolic;
 
+import src.Complex;
+
 /// A simple point
 public class Point {
     public double x;
     public double y;
+
+    public static Point ORIGIN = new Point(0,0);
 
     /// Constructor for a point
     public Point(double x, double y) {
@@ -23,5 +27,15 @@ public class Point {
 
     public Point mul(double scalar) {
         return new Point(this.x * scalar, this.y * scalar);
+    }
+
+    /// Conversion of a point to a complex number
+    public Complex to_complex() {
+        return new Complex(this.x, this.y);
+    }
+
+    /// Conversion of a complex number to a point
+    public static Point from_complex(Complex z) {
+        return new Point(z.re(), z.im());
     }
 }
