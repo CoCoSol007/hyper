d4dc2d55520aab611ff567a05307c6445ce4a7de
CoCoSol007 <solois.corentin@gmail.com>
2025-04-03 20:04:54 +0200

    Initialize main with temporary code

--- app/src/main/java/dev/cocosol/Main.java
+++ app/src/main/java/dev/cocosol/Main.java
@@ -5,19 +5,8 @@
 
 package dev.cocosol;
 
-import dev.cocosol.hyperbolic.paving.Chunk;
-import dev.cocosol.hyperbolic.paving.Direction;
-
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) {
-        Direction[] directions = new Direction[]{Direction.RIGHT, Direction.RIGHT, Direction.RIGHT, Direction.RIGHT, Direction.LEFT};
-        Chunk c = new Chunk(List.of(directions));
-        System.out.println(c);
-
-        int num = c.encode();
-        System.out.println(num);
-        System.out.println(c.getHash(678567));
+    public static void main(String[] name) {
+        System.out.println("Hi there");
     }
 }
